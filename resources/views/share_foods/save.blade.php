331f159e72c922789d6d42fa3b89bf7bca3ce50e
Hoai Thuong <thuong.hth@neo-lab.vn>
2018-03-31 15:50:10 +0700

    [9] update avatar in change password and save page

--- resources/views/share_foods/save.blade.php
+++ resources/views/share_foods/save.blade.php
@@ -14,7 +14,7 @@
               <div class="wrp">
                 <div class="avatar">
                   <div class="img">
-                    <img src="assets/img/user/h5.jpg" alt="..." />
+                    <img src="{{ $user->avatar }}" alt="..." />
                     <div class="edit-avatar" data-toggle="modal" data-target="#modal-crop-img">
                       <i class="fa fa-camera"></i>
                     </div>
@@ -85,5 +85,46 @@
       </div>
     </div>
   </div>
+</div>
+<div id="modal-crop-img" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+        <!-- Modal content-->
+        <div class="modal-content content">
+            <div class="wrp-ctn">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Edit avatar</h4>
+                </div>
+                <div class="modal-body">
+                    <form data-toggle="validator" role="form" name="form" action="{{ route('sharefood.profile.update', ['user' => $user->id]) }}" id="form-search" class="form-horizontal" enctype="multipart/form-data" method="POST">
+                        {{csrf_field()}}
+                        <div class="image-editor">
+                            <div class="cropit-preview"></div>
+                            <div class="feature-crop">
+                                <div class="col-md-4 col-xs-4 item-feature-crop">
+                                    <div class="resize">
+                                        <input type="range" class="cropit-image-zoom-input">
+                                    </div>
+                                </div>
+                                <div class="col-md-4 col-xs-4 item-feature-crop">
+                                    <div class="file btn-bg">
+                                        <i class="fa fa-camera"></i>Choose image
+                                        <input type="file" name="avatar" class="cropit-image-input btn btn-bg" />
+                                    </div>
+                                </div>
+                                <div class="col-md-4 col-xs-4 item-feature-crop">
+                                    <div class="upload">
+                                        <i class="fa fa-cloud-upload"></i>
+                                        <input type="submit" class="export btn btn-bg" value="Upload" />
+                                    </div>
+                                </div>
+                                <div class="clearfix"></div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
