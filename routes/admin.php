5f0065693309849ec53e13f1c0057e2f90b74a8e
Hoai Thuong <thuong.hth@neo-lab.vn>
2018-04-01 21:11:33 +0700

    [13] Done delete user in admin

--- routes/admin.php
+++ routes/admin.php
@@ -1,7 +1,8 @@
 <?php
 Route::group(['as' => 'admin.'], function () {
     Route::get('/', ['as' => 'index', 'uses' => 'HomeController@index']);
-    Route::group(['prefix' => 'users', 'as' => 'users'], function () {
+    Route::group(['prefix' => 'users', 'as' => 'users.'], function () {
         Route::get('/', ['as' => 'index', 'uses' => 'UserController@index']);
+        Route::get('{id}', ['as' => 'destroy', 'uses' => 'UserController@destroy']);
     });
 });
