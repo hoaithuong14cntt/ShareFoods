e0d5078db56a057a0ebef19adce2e451fc972bd5
Hoai Thuong <thuong.hth@neo-lab.vn>
2018-04-02 23:36:25 +0700

    [15] send mail not done

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -1,1 +1,6 @@
-<p>Xin chao</p>
+<h2>Phản hồi từ khách hàng.</h2>
+<p>Họ tên: <span>{{ $contact->name }}</span></p>
+<p>Phone: <span>{{ $contact->phone }}</span></p>
+<p>Địa chỉ: <span>{{ $contact->address }}</span></p>
+<p>Tiêu đề:: <span>{{ $contact->title }}</span></p>
+<p>Nội dung phản hồi: <span>{{ $contact->content }}</span></p>
