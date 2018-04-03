501eaa8ab3aff34f825b51f652a749b9344572b2
Huynh Thi Hoai Thuong <thuong.hth@neo-lab.vn>
2018-04-03 18:31:44 +0700

    [17] Show user page not done

---
+++ resources/views/admin/partials/tabs.blade.php
@@ -0,0 +1,59 @@
+<div class="panel-body">
+	@php
+		if($tab == 'favorites')
+		{
+			$user = $user->favorites()->paginate();
+		}
+
+		if($tab == 'notes')
+		{
+			$user = $user->notes()->paginate();
+		}
+
+	@endphp
+  <table class="table table-striped table-bordered bootstrap-datatable datatable">
+    <thead>
+      <tr>
+        <th>No.</th>
+        <th>Id</th>
+        <th>Tên địa điểm</th>
+        <th>Hình ảnh</th>
+        <th>Discount</th>
+        <th>Công khai</th>
+        <th>Ngày tạo</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+    @foreach($user as $key => $value)
+      <tr>
+        <td>{{ $user->firstItem() +$key }}</td>
+        <td>{{$value->id}}</td>
+        <td>{{ $value->name }}</td>
+        <td><img src="{{ $value->image }}" alt="" class="image-index"></td>
+		<td>{{ $value->discount }}</td>
+        <td>
+          <span class="label label-success">{!!published($value->is_published)!!}</span>
+        </td>
+        <td>{{ $value->created_at }}</td>
+        <td>
+          <a class="btn btn-success" href="{{ route('admin.users.show', ['user' => $value->id]) }}">
+          <i class="fa fa-search-plus "></i>
+          </a>
+          <a class="btn btn-info" href="table.html#">
+          <i class="fa fa-edit "></i>
+          </a>
+          <a class="btn btn-danger" href="{{ route('admin.users.destroy', ['user' => $value->id]) }}">
+          <i class="fa fa-trash-o "></i>
+          </a>
+        </td>
+      </tr>
+      @endforeach
+      </tbody>
+  </table>
+  <div class="pagination-outter">
+    <ul class="pagination">
+      {{ $user->appends(request()->all())->links() }}
+    </ul>
+  </div>
+</div>
